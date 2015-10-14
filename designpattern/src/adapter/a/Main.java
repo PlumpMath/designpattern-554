48b0a3f3e05272b25722dbb46b6b853596755e99
south10 <south10@webcash.co.kr>
2015-10-14 17:19:43 +0900

    project share

---
+++ designpattern/src/adapter/a/Main.java
@@ -0,0 +1,9 @@
+package adapter.a;
+
+public class Main {
+	public static void main(String[] args) {
+		Print p = new PrintBanner("Hello");
+		p.printWeak();
+		p.printStrong();
+	}
+}
---
+++ designpattern/src/adapter/a/Print.java
@@ -0,0 +1,6 @@
+package adapter.a;
+
+public interface Print {
+	public void printWeak();
+    public void printStrong();
+}
