6a7b0582120a3e23cc2d838e85452d8ab0311de1
insukChoi <cis1725@gmail.com>
2015-10-20 17:09:58 +0900

    디자인패턴 4과 Factory Method

---
+++ designpattern/src/study/insuk/factory/framework/Product.java
@@ -0,0 +1,6 @@
+package study.insuk.factory.framework;
+
+public abstract class Product {
+ 
+	public abstract void use();
+}
--- designpattern/src/templatemethod/Main.java
+++ designpattern/src/templatemethod/Main.java
@@ -4,11 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		AbstractDisplay d1 = new CharDisplay('H');
 		AbstractDisplay d2 = new StringDisplay("Hello, world.");
-		AbstractDisplay d3 = new StringDisplay("ì•ˆë…•í•˜ì„¸ìš”");
+		AbstractDisplay d3 = new StringDisplay("?•ˆ?…•?•˜?„¸?š”");
 		
 		d1.display();
 		d2.display();
 		d3.display();
 	}
-	
 }
