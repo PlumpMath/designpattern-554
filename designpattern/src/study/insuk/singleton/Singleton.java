f4e84e2186eb468245281d8c33d2dd194e90e29e
insuk <insuk@218.38.137.27>
2015-11-07 23:03:18 +0900

    Singleton 패턴 예제1

---
+++ designpattern/src/study/insuk/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package study.insuk.singleton;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	public Singleton() {
+		System.out.println("인스턴스를 생성했습니다.");
+	}
+	public static Singleton getInsance(){
+		return singleton;
+	}
+}
